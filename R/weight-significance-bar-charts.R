dc33048c494b468297fe44f200914d5c0476f452
unmnn <uli.niemann@ovgu.de>
2021-05-24 14:49:13 +0200

    consistent number of significant digits

--- R/weight-significance-bar-charts.R
+++ R/weight-significance-bar-charts.R
@@ -1,17 +1,29 @@
 source("R/general-stuff.R")
 
 df <- dir("data", pattern = "-count\\.rds", full.names = TRUE) %>%
-  map_dfr(read_rds)
+  map_dfr(read_rds) 
+df <-
+  df %>%
+  mutate(chr_opening = str_extract(weight_disc, "^.")) %>%
+  mutate(chr_first = str_replace(weight_disc, "^.(.+),.*", "\\1")) %>%
+  mutate(chr_second = str_replace(weight_disc, "^.+,(.+).$", "\\1")) %>%
+  mutate(chr_closing = str_extract(weight_disc, ".$")) %>%
+  mutate(across(c(chr_first, chr_second), ~ format(as.numeric(.x), nsmall = 3))) %>%
+  mutate(weight_disc2 = glue::glue("{chr_opening}{chr_first}, {chr_second}{chr_closing}")) %>%
+  mutate(weight_disc2 = fct_inorder(weight_disc2))
 df
 
-ggplot(df, aes(x = n, y = weight_disc, fill = sig_disc)) +
+ggplot(df, aes(x = n, y = weight_disc2, fill = sig_disc)) +
   scale_x_continuous(expand = c(0.015,0), labels = function(x) x * 100) +
   scale_y_discrete(expand = c(0,0.5,0,0)) +
   geom_col(position = "fill", width = 0.775) +
   facet_wrap(~ q, scales = "free_y", nrow = 1) +
   geom_text(
-    data = df %>% filter(q == "MDI", weight_disc == "(1.4,11.2]") %>% mutate(perc =  1 - (0.5 * n / sum(n) + lag(n / sum(n), default = 0))) %>% mutate(label = c("pruned", "kept")),
-    aes(x = perc, y = weight_disc, label = label),
+    data = df %>% 
+      filter(q == "MDI", weight_disc2 == "(1.400, 11.200]") %>% 
+      mutate(perc =  1 - (0.5 * n / sum(n) + lag(n / sum(n), default = 0))) %>% 
+      mutate(label = c("pruned", "kept")),
+    aes(x = perc, y = weight_disc2, label = label),
     size = 8/.pt, fontface = "italic"
   ) +
   guides(fill = FALSE) +
@@ -25,8 +37,9 @@ ggplot(df, aes(x = n, y = weight_disc, fill = sig_disc)) +
   theme(panel.grid.major.x = element_line(linetype = "dashed", color = "gray70")) +
   theme(strip.text = element_text(size = rel(1), color = "black")) +
   theme(strip.background = element_rect(color = NA, fill = "gray90")) +
-  theme(panel.spacing.x = unit(0.35, "cm")) +
-  # theme(axis.text.y = element_text(size = rel(1))) +
+  theme(panel.spacing.x = unit(0.25, "cm")) +
+  theme(axis.text.x = element_text(margin = margin(t = -0.05, unit = "mm"))) +
+  theme(axis.text.y = element_text(margin = margin(r = -0.25, unit = "mm"))) +
   theme(plot.margin = margin(1,2,1,1,"mm"))
 
 ggsave("figures/weight-significance-bar-charts.png", width = 18, height = 4.5, units = "cm", dpi = 600)
